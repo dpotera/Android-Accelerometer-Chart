1bc2f5b6345d9cccad273dbcfa26bf2a870be953
dpotera <dominik.potera@gmail.com>
2017-06-08 00:42:42 +0200

    Added AndroidPlot lib & simple chart in main activity

--- app/src/main/java/pl/potera/lightsword/MainActivity.kt
+++ app/src/main/java/pl/potera/lightsword/MainActivity.kt
@@ -1,5 +1,6 @@
 package pl.potera.lightsword
 
+import android.hardware.Sensor
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
@@ -12,8 +13,30 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
+import android.hardware.SensorManager
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import com.androidplot.xy.XYGraphWidget
+import com.androidplot.xy.CatmullRomInterpolator
+import android.graphics.DashPathEffect
+import com.androidplot.xy.LineAndPointFormatter
+import com.androidplot.xy.SimpleXYSeries
+import com.androidplot.xy.XYPlot
+import com.androidplot.util.PixelUtils;
+import com.androidplot.xy.*
+import java.text.FieldPosition
+import java.text.Format
+import java.text.ParsePosition
+import java.util.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
+        SensorEventListener {
+    private val alpha = 0.8f
+    private val gravity = FloatArray(3)
+    private val linear_acceleration = FloatArray(3)
+    private val plot: XYPlot? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +58,74 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
         val navigationView = findViewById(R.id.nav_view) as NavigationView
         navigationView.setNavigationItemSelectedListener(this)
+
+        val mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        val mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+
+
+        initializeChart()
+    }
+
+    private fun initializeChart() {
+        // initialize our XYPlot reference:
+        val plot = findViewById(R.id.plot) as XYPlot
+
+        // create a couple arrays of y-values to plot:
+        val domainLabels = arrayOf<Number>(1, 2, 3, 6, 7, 8, 9, 10, 13, 14)
+        val series1Numbers = mutableListOf<Number>(1, 4, 2, 8, 4, 16, 8, 32, 16, 64)
+        val series2Numbers = mutableListOf<Number>(5, 2, 10, 5, 20, 10, 50, 20, 80, 40)
+
+        // turn the above arrays into XYSeries':
+        // (Y_VALS_ONLY means use the element index as the x value)
+        val series1 = SimpleXYSeries(series1Numbers , SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1")
+        val series2 = SimpleXYSeries(series2Numbers, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2")
+
+        // create formatters to use for drawing a series using LineAndPointRenderer
+        // and configure them from xml:
+        val series1Format = LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels)
+
+        val series2Format = LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels_2)
+
+        // add an "dash" effect to the series2 line:
+                // always use DP when specifying pixel sizes, to keep things consistent across devices:
+        series2Format.linePaint.pathEffect = DashPathEffect(floatArrayOf(
+                PixelUtils.dpToPix(20f), PixelUtils.dpToPix(15f)), 0f)
+
+        // just for fun, add some smoothing to the lines:
+        // see: http://androidplot.com/smooth-curves-and-androidplot/
+        series1Format.interpolationParams = CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+
+        series2Format.interpolationParams = CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+
+        // add a new series' to the xyplot:
+        plot.addSeries(series1, series1Format)
+        plot.addSeries(series2, series2Format)
+
+        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
+            override fun format(obj: Any, toAppendTo: StringBuffer, pos: FieldPosition): StringBuffer {
+                val i = Math.round((obj as Number).toFloat())
+                return toAppendTo.append(domainLabels[i])
+            }
+
+            override fun parseObject(source: String, pos: ParsePosition): Any? {
+                return null
+            }
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
+
+        linear_acceleration[0] = event.values[0] - gravity[0]
+        linear_acceleration[1] = event.values[1] - gravity[1]
+        linear_acceleration[2] = event.values[2] - gravity[2]
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onBackPressed() {
