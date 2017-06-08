0537c7e95e19e77647965abbbe19703885709407
dpotera <dominik.potera@gmail.com>
2017-06-08 03:11:57 +0200

    Working accelerometer chart

--- app/src/main/java/pl/potera/lightsword/MainActivity.kt
+++ app/src/main/java/pl/potera/lightsword/MainActivity.kt
@@ -1,10 +1,10 @@
 package pl.potera.lightsword
 
+import android.graphics.Color
 import android.hardware.Sensor
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.Snackbar
-import android.view.View
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
@@ -16,27 +16,26 @@ import android.view.MenuItem
 import android.hardware.SensorManager
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
-import com.androidplot.xy.XYGraphWidget
-import com.androidplot.xy.CatmullRomInterpolator
-import android.graphics.DashPathEffect
-import com.androidplot.xy.LineAndPointFormatter
-import com.androidplot.xy.SimpleXYSeries
-import com.androidplot.xy.XYPlot
-import com.androidplot.util.PixelUtils;
+import com.androidplot.Plot
+import com.androidplot.util.PlotStatistics
+import com.androidplot.util.Redrawer
 import com.androidplot.xy.*
-import java.text.FieldPosition
-import java.text.Format
-import java.text.ParsePosition
+import kotlinx.android.synthetic.main.content_main.*
+import java.text.DecimalFormat
 import java.util.*
-
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener,
         SensorEventListener {
     private val alpha = 0.8f
     private val gravity = FloatArray(3)
     private val linear_acceleration = FloatArray(3)
-    private val plot: XYPlot? = null
 
+    private val xHistorySeries: SimpleXYSeries = SimpleXYSeries("X")
+    private val yHistorySeries: SimpleXYSeries = SimpleXYSeries("Y")
+    private val zHistorySeries: SimpleXYSeries = SimpleXYSeries("Z")
+
+    private var sensorManager: SensorManager? = null
+    private var redrawer: Redrawer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,71 +49,54 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                     .setAction("Action", null).show()
         }
 
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
         val toggle = ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer.setDrawerListener(toggle)
+        drawer.addDrawerListener(toggle)
         toggle.syncState()
 
         val navigationView = findViewById(R.id.nav_view) as NavigationView
         navigationView.setNavigationItemSelectedListener(this)
 
-        val mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        val mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
-
-
         initializeChart()
     }
 
     private fun initializeChart() {
-        // initialize our XYPlot reference:
-        val plot = findViewById(R.id.plot) as XYPlot
+        xHistorySeries.useImplicitXVals()
+        yHistorySeries.useImplicitXVals()
+        zHistorySeries.useImplicitXVals()
 
-        // create a couple arrays of y-values to plot:
-        val domainLabels = arrayOf<Number>(1, 2, 3, 6, 7, 8, 9, 10, 13, 14)
-        val series1Numbers = mutableListOf<Number>(1, 4, 2, 8, 4, 16, 8, 32, 16, 64)
-        val series2Numbers = mutableListOf<Number>(5, 2, 10, 5, 20, 10, 50, 20, 80, 40)
+        history_plot.setRangeBoundaries(-50, 50, BoundaryMode.FIXED)
+        history_plot.linesPerRangeLabel = 5
+        history_plot.setRangeStep(StepMode.INCREMENT_BY_FIT, 10.0)
+        history_plot.setRangeLabel("Acceleration (-g - ∑F / mass)")
+        history_plot.rangeTitle.pack()
 
-        // turn the above arrays into XYSeries':
-        // (Y_VALS_ONLY means use the element index as the x value)
-        val series1 = SimpleXYSeries(series1Numbers , SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series1")
-        val series2 = SimpleXYSeries(series2Numbers, SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, "Series2")
+        history_plot.setDomainBoundaries(0, HISTORY_SIZE, BoundaryMode.FIXED)
+        history_plot.setDomainLabel("Dimensions")
+        history_plot.domainTitle.pack()
 
-        // create formatters to use for drawing a series using LineAndPointRenderer
-        // and configure them from xml:
-        val series1Format = LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels)
+        history_plot.addSeries(xHistorySeries, LineAndPointFormatter(Color.rgb(100, 100, 200), null, null, null))
+        history_plot.addSeries(yHistorySeries, LineAndPointFormatter(Color.rgb(100, 200, 100), null, null, null))
+        history_plot.addSeries(zHistorySeries, LineAndPointFormatter(Color.rgb(200, 100, 100), null, null, null))
+        history_plot.domainStepMode = StepMode.INCREMENT_BY_VAL
+        history_plot.domainStepValue = (HISTORY_SIZE / 10).toDouble()
 
-        val series2Format = LineAndPointFormatter(this, R.xml.line_point_formatter_with_labels_2)
+        history_plot.graph.getLineLabelStyle(XYGraphWidget.Edge.LEFT).format = DecimalFormat("#")
+        history_plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = DecimalFormat("#")
 
-        // add an "dash" effect to the series2 line:
-                // always use DP when specifying pixel sizes, to keep things consistent across devices:
-        series2Format.linePaint.pathEffect = DashPathEffect(floatArrayOf(
-                PixelUtils.dpToPix(20f), PixelUtils.dpToPix(15f)), 0f)
+        val histStats = PlotStatistics(1000, false)
+        history_plot.addListener(histStats)
 
-        // just for fun, add some smoothing to the lines:
-        // see: http://androidplot.com/smooth-curves-and-androidplot/
-        series1Format.interpolationParams = CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
+        val mAccelerometer = sensorManager?.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        sensorManager?.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_UI)
 
-        series2Format.interpolationParams = CatmullRomInterpolator.Params(10, CatmullRomInterpolator.Type.Centripetal)
-
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format)
-        plot.addSeries(series2, series2Format)
-
-        plot.getGraph().getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = object : Format() {
-            override fun format(obj: Any, toAppendTo: StringBuffer, pos: FieldPosition): StringBuffer {
-                val i = Math.round((obj as Number).toFloat())
-                return toAppendTo.append(domainLabels[i])
-            }
-
-            override fun parseObject(source: String, pos: ParsePosition): Any? {
-                return null
-            }
-        }
+        redrawer = Redrawer(Arrays.asList(*arrayOf<Plot<*, *, *, *, *>>(history_plot)), 100f, false)
     }
 
-    override fun onSensorChanged(event: SensorEvent) {
+    @Synchronized override fun onSensorChanged(event: SensorEvent) {
         gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0]
         gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1]
         gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2]
@@ -122,10 +104,20 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         linear_acceleration[0] = event.values[0] - gravity[0]
         linear_acceleration[1] = event.values[1] - gravity[1]
         linear_acceleration[2] = event.values[2] - gravity[2]
+
+        if (xHistorySeries.size() > HISTORY_SIZE) {
+            xHistorySeries.removeFirst()
+            yHistorySeries.removeFirst()
+            zHistorySeries.removeFirst()
+        }
+
+        xHistorySeries.addLast(null, linear_acceleration[0])
+        yHistorySeries.addLast(null, linear_acceleration[1])
+        zHistorySeries.addLast(null, linear_acceleration[2])
     }
 
     override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //  not implemented
     }
 
     override fun onBackPressed() {
@@ -148,12 +140,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         val id = item.itemId
-
-
-        if (id == R.id.action_settings) {
-            return true
-        }
-
+        if (id == R.id.action_settings) return true
         return super.onOptionsItemSelected(item)
     }
 
@@ -178,5 +165,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout
         drawer.closeDrawer(GravityCompat.START)
         return true
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        redrawer?.start()
+    }
+
+    public override fun onPause() {
+        redrawer?.pause()
+        super.onPause()
+    }
+
+    public override fun onDestroy() {
+        redrawer?.finish()
+        super.onDestroy()
+    }
+
+    companion object {
+        private val HISTORY_SIZE = 30
     }
 }
